6a1e39711fa9054a7b9c64b5f7e3f882e4d42408
Sarah Siqueira <anonimedsarah@outlook.com>
2021-05-15 15:43:20 -0300

    Estética da tela de login

---
+++ OlharDeMenina/Login.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace OlharDeMenina
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pic_senha = new System.Windows.Forms.PictureBox();
+            this.pic_user = new System.Windows.Forms.PictureBox();
+            this.pic_olho = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtB_senha2 = new System.Windows.Forms.TextBox();
+            this.btn_login_entrar = new System.Windows.Forms.Button();
+            this.txtB_senha1 = new System.Windows.Forms.TextBox();
+            this.txtB_nome = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnl_superior = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_senha)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_user)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_olho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnl_superior.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(-1, 290);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(418, 180);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panel2.Controls.Add(this.pic_senha);
+            this.panel2.Controls.Add(this.pic_user);
+            this.panel2.Controls.Add(this.pic_olho);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.txtB_senha2);
+            this.panel2.Controls.Add(this.btn_login_entrar);
+            this.panel2.Controls.Add(this.txtB_senha1);
+            this.panel2.Controls.Add(this.txtB_nome);
+            this.panel2.Location = new System.Drawing.Point(55, 241);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(306, 271);
+            this.panel2.TabIndex = 1;
+            // 
+            // pic_senha
+            // 
+            this.pic_senha.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pic_senha.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_senha.Image = global::OlharDeMenina.Properties.Resources.abrir_fechadura;
+            this.pic_senha.Location = new System.Drawing.Point(30, 100);
+            this.pic_senha.Name = "pic_senha";
+            this.pic_senha.Size = new System.Drawing.Size(32, 26);
+            this.pic_senha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pic_senha.TabIndex = 11;
+            this.pic_senha.TabStop = false;
+            // 
+            // pic_user
+            // 
+            this.pic_user.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pic_user.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_user.Image = global::OlharDeMenina.Properties.Resources.user1;
+            this.pic_user.Location = new System.Drawing.Point(30, 49);
+            this.pic_user.Name = "pic_user";
+            this.pic_user.Size = new System.Drawing.Size(32, 26);
+            this.pic_user.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pic_user.TabIndex = 10;
+            this.pic_user.TabStop = false;
+            // 
+            // pic_olho
+            // 
+            this.pic_olho.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pic_olho.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic_olho.Image = global::OlharDeMenina.Properties.Resources.olho__1_;
+            this.pic_olho.Location = new System.Drawing.Point(249, 100);
+            this.pic_olho.Name = "pic_olho";
+            this.pic_olho.Size = new System.Drawing.Size(32, 26);
+            this.pic_olho.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pic_olho.TabIndex = 9;
+            this.pic_olho.TabStop = false;
+            this.pic_olho.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_olho_MouseDown);
+            this.pic_olho.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_olho_MouseUp);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.panel4.Location = new System.Drawing.Point(30, 132);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(251, 1);
+            this.panel4.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.panel3.Location = new System.Drawing.Point(30, 81);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(251, 1);
+            this.panel3.TabIndex = 7;
+            // 
+            // txtB_senha2
+            // 
+            this.txtB_senha2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtB_senha2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtB_senha2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB_senha2.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.txtB_senha2.HideSelection = false;
+            this.txtB_senha2.Location = new System.Drawing.Point(68, 107);
+            this.txtB_senha2.Name = "txtB_senha2";
+            this.txtB_senha2.PasswordChar = '*';
+            this.txtB_senha2.Size = new System.Drawing.Size(201, 19);
+            this.txtB_senha2.TabIndex = 5;
+            this.txtB_senha2.TabStop = false;
+            this.txtB_senha2.Text = "Senha";
+            this.txtB_senha2.Click += new System.EventHandler(this.txtB_senha2_Click);
+            // 
+            // btn_login_entrar
+            // 
+            this.btn_login_entrar.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.btn_login_entrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_login_entrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login_entrar.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login_entrar.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.btn_login_entrar.Location = new System.Drawing.Point(89, 196);
+            this.btn_login_entrar.Name = "btn_login_entrar";
+            this.btn_login_entrar.Size = new System.Drawing.Size(122, 45);
+            this.btn_login_entrar.TabIndex = 2;
+            this.btn_login_entrar.Text = "Login";
+            this.btn_login_entrar.UseVisualStyleBackColor = false;
+            // 
+            // txtB_senha1
+            // 
+            this.txtB_senha1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtB_senha1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtB_senha1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB_senha1.Location = new System.Drawing.Point(68, 107);
+            this.txtB_senha1.Name = "txtB_senha1";
+            this.txtB_senha1.Size = new System.Drawing.Size(201, 19);
+            this.txtB_senha1.TabIndex = 1;
+            // 
+            // txtB_nome
+            // 
+            this.txtB_nome.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtB_nome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtB_nome.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB_nome.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.txtB_nome.HideSelection = false;
+            this.txtB_nome.Location = new System.Drawing.Point(68, 56);
+            this.txtB_nome.Name = "txtB_nome";
+            this.txtB_nome.Size = new System.Drawing.Size(201, 19);
+            this.txtB_nome.TabIndex = 0;
+            this.txtB_nome.TabStop = false;
+            this.txtB_nome.Text = "Nome";
+            this.txtB_nome.Click += new System.EventHandler(this.txtB_nome_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(97, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 192);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.Transparent;
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_close.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_close.Location = new System.Drawing.Point(375, 1);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(35, 33);
+            this.btn_close.TabIndex = 3;
+            this.btn_close.Text = "X";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pnl_superior
+            // 
+            this.pnl_superior.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_superior.Controls.Add(this.btn_close);
+            this.pnl_superior.Location = new System.Drawing.Point(-1, 0);
+            this.pnl_superior.Name = "pnl_superior";
+            this.pnl_superior.Size = new System.Drawing.Size(418, 37);
+            this.pnl_superior.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(410, 546);
+            this.Controls.Add(this.pnl_superior);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.TopMost = true;
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_senha)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_user)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_olho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnl_superior.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_login_entrar;
+        private System.Windows.Forms.TextBox txtB_senha1;
+        private System.Windows.Forms.TextBox txtB_nome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.TextBox txtB_senha2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pic_olho;
+        private System.Windows.Forms.PictureBox pic_senha;
+        private System.Windows.Forms.PictureBox pic_user;
+        private System.Windows.Forms.Panel pnl_superior;
+    }
+}
