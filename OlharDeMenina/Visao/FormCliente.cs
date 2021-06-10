5e24da04eac9e4ab0a286ae5b8e0d25e44faf06d
vitorpdsg <vitorpdsg@gmail.com>
2021-06-10 20:27:41 -0300

    Avanços na função de busca na aba clientes.

--- OlharDeMenina/Visao/FormCliente.cs
+++ OlharDeMenina/Visao/FormCliente.cs
@@ -12,15 +12,25 @@ namespace OlharDeMenina
         {
             InitializeComponent();
         }
+        //solução para a busca da internet que não foi finalizada. depois vou tentar elaborar nisso, mas fica aí pra história. ass: Vitor
+        string[] nomeLista = { };
+        string[] CPFLista = { };
+        string[] telefoneLista = { };
+        int index = 0;
+
         public int idClien;
         private void btn_AdicionarC_Click(object sender, EventArgs e)
         {
+            
             ControleCliente cc = new ControleCliente();
             Clientes clientes = new Clientes(tbox_nome.Text, tbox_cpf.Text, tbox_telefone.Text, tbox_endereco.Text, tbox_datadenascimento.Text);
             cc.AdicionarClientes(clientes);
-
+            //nomeLista[index] = tbox_nome.Text;
+            //CPFLista[index] = tbox_cpf.Text;
+            telefoneLista[index] = tbox_telefone.Text;
             PreencherListView();
             LimparCampos();
+            //index += 1 ;
         }
 
         private void btn_ExcluirC_Click(object sender, EventArgs e)
@@ -89,6 +99,7 @@ namespace OlharDeMenina
             PreencherListView();
             LimparCampos();
         }
+        
 
         private void listView_funf_Click(object sender, EventArgs e)
         {
@@ -107,5 +118,35 @@ namespace OlharDeMenina
                     tbox_datadenascimento.Text = dr.GetString(5);
                 }
             }
+
+        private void botaoBuscar_Click_1(object sender, EventArgs e)
+        {
+            // isso aqui é vindo do banco de dados para fazer a pesquisa. é uma solução, mas meio gambiarra kk ass: Vitor
+            
+                try
+                {
+                    ControleCliente cf = new ControleCliente();
+                    MySqlDataReader dr = cf.RetornarClientes(Int32.Parse(caixaBuscar.Text));
+
+                    if (dr != null)
+                    {
+                        dr.Read();
+
+                        tbox_nome.Text = dr.GetString(1);
+                        tbox_cpf.Text = dr.GetString(2);
+                        tbox_telefone.Text = dr.GetString(3);
+                        tbox_endereco.Text = dr.GetString(4);
+
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Erro: " + ex);
+                }
+
+            
         }
+    }
+        
+
     }
